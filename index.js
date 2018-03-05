c2fe05f67578652edfefba253740d2ff40079d56
arwa alharbi <arwanh@mit.edu>
2018-03-05 13:53:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var name = "susan"
 var height = "74"
-if (name === "susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan") 
-}
+
+
 // Don't worry about this
 module.exports = { name, height
 }
